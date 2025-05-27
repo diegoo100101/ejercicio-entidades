b736435f053dc0f779c20ba029ba5f3ca1366e00
Diego Cantoral <diego.cantoral@axity.com>
2025-05-27 14:19:15 -0600

    modified ruta

--- src/main/java/com/ejercicioEntidades/entidades/model/Ruta.java
+++ src/main/java/com/ejercicioEntidades/entidades/model/Ruta.java
@@ -1,7 +1,18 @@
 package com.ejercicioEntidades.entidades.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
-
+@Entity
+@Getter
+@Setter
 public class Ruta implements Serializable {
+
+    @Id
+    @Column(name = "id_ruta")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRuta;
 }
